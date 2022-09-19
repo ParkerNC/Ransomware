ae75ff9bce26c0ca725c46e47dd432486ebbc96a
Parker Collier <pcollie4@vols.utk.edu>
2022-09-19 14:58:59 -0400

    server updates

--- cnc/server.py
+++ cnc/server.py
@@ -29,28 +29,18 @@ class ClientThread(Thread):
             
             print(f"recieved user connectinon: {data}")
 
-            key1 = Fernet.generate_key()
-
-            self.conn.send(key1)
-
-            x = input()
-            if x == 'send':
-                self.conn.send(b'XD')
-
-
-            
-
-
 
 class Server():
     def __init__(self, host: str, port: int) -> None:
         self.ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.ss.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.ss.bind((host, port))
-        self.ss.listen(1)
+        self.ss.listen(10)
         self.host = host
         self.port = port
-        self.threads = []
+        self.threads = {}
+        self.inp = ''
+        self.Users = os.scandir("Users")
 
     def keyGen(self, id: str) -> bytes:
         key = Fernet.generate_key()
@@ -58,22 +48,58 @@ class Server():
             gotUser.write(key)
         return key
 
-    
+    def thread_check(self, new_thread: ClientThread) -> None:
+        print(new_thread.id)
+        print(self.Users)
+        for t in self.Users:
+            print(t)
+            if new_thread.id == t:
+                new_thread.conn.send(b"wait")
+                return
         
+        new_thread.conn.send(self.keyGen(new_thread.id))
+    
     def start(self) -> None:
         r_socks, w_socks, e_socks = select.select([self.ss], [], [])
         while True:
-            print("waiting")
             for sock in r_socks:
                 (conn, (self.host, self.port)) = self.ss.accept()
                 new_thread = ClientThread(self.host, self.port, conn)
                 new_thread.start()
-                self.threads.append(new_thread)
+
+                if new_thread.id == None:
+                    print("bad")
+                    continue
+                
+                userId = new_thread.id.decode("Utf-8")
+                print(userId)
+                self.thread_check(new_thread)
+                self.threads[new_thread.id] = new_thread
+
+    def inputThread(self) -> str:
+        while 1:
+            self.inp = input(">: ")
+            print(self.inp)
+            if self.inp == "kill":
+                return
+
+    def inputLoop(self) -> None:
+        
+        inputThread = Thread(target=self.inputThread)
+        inputThread.start()
+
+        self.start()
+
+        inputThread.join()
+        
+
+
+
 
 
 if __name__ == "__main__":
 
     s = Server("127.0.0.1", 5789)
-    s.start()
-    
+    s.inputLoop()
+
     sys.exit()
